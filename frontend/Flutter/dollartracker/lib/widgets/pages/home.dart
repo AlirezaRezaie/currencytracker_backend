d7e4150ab2d2df83d0e216dd876de7540658d397
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-07-28 22:20:02 +0330

    added websocket to flutter home

--- frontend/Flutter/dollartracker/lib/widgets/pages/home.dart
+++ frontend/Flutter/dollartracker/lib/widgets/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +10,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late WebSocketChannel channel;
+  String receivedData = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // Replace 'ws://your_websocket_url' with your actual WebSocket server URL.
+    channel = IOWebSocketChannel.connect('ws://192.168.1.6:8000/live');
+    channel.stream.listen((data) {
+      setState(() {
+        receivedData = data;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,16 +41,16 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.only(right: 20, left: 20),
           child: Container(
             decoration: BoxDecoration(
-              color: Color.fromARGB(255, 0, 151, 230),
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: const Padding(
+                color: Color.fromARGB(255, 0, 151, 230),
+                borderRadius: BorderRadius.circular(10)),
+            child: Padding(
               padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
               child: Column(
                 children: [
                   Text(
-                    "The Price of Dollar",
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    receivedData,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
                   )
                 ],
               ),
