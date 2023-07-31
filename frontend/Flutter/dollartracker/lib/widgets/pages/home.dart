5098c9daa36050cacc848f2621bae72971f0a407
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-07-31 14:31:32 +0330

    maintain persistant connection with websocket server

--- frontend/Flutter/dollartracker/lib/widgets/pages/home.dart
+++ frontend/Flutter/dollartracker/lib/widgets/pages/home.dart
@@ -13,19 +13,55 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late WebSocketChannel channel;
   String receivedData = '';
+  bool isConnecting = false;
+  String errorMessage = '';
 
   @override
   void initState() {
     String host = dotenv.env['SERVER_HOST'] ?? 'localhost';
     super.initState();
     // Replace 'ws://your_websocket_url' with your actual WebSocket server URL.
-    channel = IOWebSocketChannel.connect(host);
-    channel.stream.listen((data) {
-      print(data);
-      setState(() {
-        receivedData = data;
-      });
+    _connectToWebSocket(host);
+  }
+
+  void _connectToWebSocket(String host) async {
+    setState(() {
+      isConnecting = true;
+      errorMessage = '';
     });
+
+    channel = IOWebSocketChannel.connect(host);
+    channel.stream.listen(
+      (data) {
+        print(data);
+        setState(() {
+          receivedData = data;
+        });
+      },
+      onDone: () {
+        print("WebSocket disconnected");
+        _reconnectToWebSocket(host);
+      },
+      onError: (error) {
+        print("WebSocket error: $error");
+        setState(() {
+          errorMessage = "WebSocket error: $error";
+        });
+        _reconnectToWebSocket(host);
+      },
+    );
+
+    setState(() {
+      isConnecting = false;
+    });
+  }
+
+  void _reconnectToWebSocket(String host) {
+    if (!isConnecting) {
+      Future.delayed(Duration(seconds: 5), () {
+        _connectToWebSocket(host);
+      });
+    }
   }
 
   @override
@@ -44,8 +80,9 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.only(right: 20, left: 20),
           child: Container(
             decoration: BoxDecoration(
-                color: Color.fromARGB(255, 0, 151, 230),
-                borderRadius: BorderRadius.circular(10)),
+              color: Color.fromARGB(255, 0, 151, 230),
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: Padding(
               padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
               child: Column(
@@ -53,8 +90,27 @@ class _HomeState extends State<Home> {
                   Text(
                     receivedData,
                     style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  )
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  isConnecting
+                      ? Text(
+                          "Connecting...",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      : SizedBox(),
+                  errorMessage.isNotEmpty
+                      ? Text(
+                          errorMessage,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : SizedBox(),
                 ],
               ),
             ),
