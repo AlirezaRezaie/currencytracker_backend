484128f88567d94b859831c78fa370e7e72c15fd
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-08-11 22:31:00 +0330

    merge conflict error fix

--- frontend/Flutter/dollartracker/lib/main.dart
+++ frontend/Flutter/dollartracker/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'widgets/pages/home.dart';
+import 'app.dart';
 
 void main() async {
   await dotenv.load();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: MyApp(),
   ));
 }
