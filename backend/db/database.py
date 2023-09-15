8f13f87d64546daf6dcbf40f0e15f4f0e099ccd2
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-15 19:01:11 +0330

    update db path

--- backend/db/database.py
+++ backend/db/database.py
@@ -5,7 +5,7 @@ from db.models import Base
 
 # from db.test_db import add_news
 # Create the tables in your database
-engine = create_engine("sqlite:///db/your_database.db")
+engine = create_engine("sqlite:///./db/your_database.db")
 Base.metadata.create_all(engine)
 
 # Now that the tables are created, configure your engine and metadata
