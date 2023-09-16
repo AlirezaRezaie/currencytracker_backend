be126d7189fcbf85d34233bcbe64ba8d1fcc941e
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-16 22:18:06 +0330

    added jdate

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
+import jdatetime
+import locale
+
+locale.setlocale(locale.LC_ALL, "fa_IR")
 
 Base = declarative_base()
 
@@ -11,7 +14,10 @@ class News(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255, collation="utf8mb4_unicode_ci"), index=True)
     topic = Column(String(255, collation="utf8mb4_unicode_ci"), index=True)
-    description = Column(String(255, collation="utf8mb4_unicode_ci"), index=True)
+    description = Column(String(2000, collation="utf8mb4_unicode_ci"), index=True)
     image_link = Column(String(255), index=True)
     time_to_read = Column(Integer, index=True)
-    created_at = Column(DateTime, default=datetime.datetime.now)
+    created_at = Column(
+        String(30, collation="utf8mb4_unicode_ci"),
+        default=jdatetime.datetime.now().strftime("%a, %d %b %Y %H:%M"),
+    )
