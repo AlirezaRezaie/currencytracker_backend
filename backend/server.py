84a19e67726e81672f770401cea098d63dfc8a3a
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-19 22:37:24 +0330

    startup and shutdown logic for better handling

--- backend/server.py
+++ backend/server.py
@@ -4,8 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from network import network_stability_check
 from logs import logger
 from routes import counter, live, news, utils
+from tasks import Task, tasks
 from settings import *
-
 import locale
 
 locale.setlocale(locale.LC_ALL, "fa_IR")
@@ -18,6 +18,25 @@ app.include_router(live.router, prefix="/api", tags=["live"])
 app.include_router(counter.router, prefix="/counter", tags=["counter"])
 app.include_router(news.router, prefix="/news", tags=["news"])
 app.include_router(utils.router, prefix="/utils", tags=["utils"])
+
+
+@app.on_event("startup")
+async def startup_event():
+    # start the default channels on startup :O
+    usd = Task("USD")
+    dhs = Task("DHS")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    # start the default channels on startup :O
+    print("bye")
+    print(len(tasks))
+    for task in tasks:
+        task.stop_event.set()
+        while True:
+            task.task.join()
+            break
 
 
 if __name__ == "__main__":
