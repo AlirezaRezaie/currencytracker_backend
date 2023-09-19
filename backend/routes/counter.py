296221201db1caa2b9081754f73259c0eaab3543
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-19 16:17:12 +0330

    changed counter route

--- backend/routes/counter.py
+++ backend/routes/counter.py
@@ -1,13 +1,22 @@
 from modes import run_counter
-from tasks import *
+from tasks import Arg
+from settings import get_defaults
 from locals import local
 from fastapi import APIRouter
+
 
 router = APIRouter()
 
 
-@router.get("/get_last/{id}/{count}")
-async def get_live_counter(id: str, count: int) -> str:
-    args = Arg(id, count=count)
+@router.get("/get_last/{code}/{count}")
+async def get_live_counter(code: str, count: int) -> str:
+    default_id = get_defaults(code)
+    args = Arg(default_id, count=count)
     local.args = args
     return str(run_counter(args))
+
+
+@router.get("/get_supported")
+async def get_live_counter(code: str, count: int) -> list:
+    defaults = get_defaults()
+    return list(defaults.values())
