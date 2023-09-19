40d9a6f6cba256a5d8f6ad7c2ecd5d2b48469d24
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-19 22:36:43 +0330

    move thread local variables to a better place

--- backend/modes.py
+++ backend/modes.py
@@ -1,24 +1,26 @@
 from network import fetch_price_data_u_preview_page as fetch_function
-from network import priceInfo
 from price import extract_prices
 from logs import logger
 from locals import local
 
 
 def run_live(emmitter_callback, error_callback, stop_event, args=None):
+    local.channel_id = args.channel_info["channel_name"]
+    local.channel_info = args.channel_info
     local.args = args
+
     server_mode = "live"
     prev_fetch = []
     last_price = None
     patience = 4
     while not stop_event.is_set():
         curr_fetch = None
-        priceInfo.channels_last_post_number[args.channel_id] = 0
+        local.last_post_number = 0
 
         # we fetch untill we reach some prices we can change the patience to be more it will search more messages
         for _ in range(patience):
             curr_fetch_nullable = extract_prices(
-                fetch_function(server_mode, args), reverse=True
+                fetch_function(server_mode), reverse=True
             )
 
             if curr_fetch_nullable:
@@ -45,7 +47,7 @@ def run_live(emmitter_callback, error_callback, stop_event, args=None):
                 if not price == last_price:
                     price.calculate_and_set_rate_of_change(last_price)
                     to_user = price.get_json_data()
-                    emmitter_callback(to_user, args.channel_id)
+                    emmitter_callback(to_user, args.code)
                     last_price = price
 
         else:
@@ -55,22 +57,22 @@ def run_live(emmitter_callback, error_callback, stop_event, args=None):
     print("FINISH LOOP")
 
 
-def run_counter(args):
-    priceInfo.channels_last_post_number[args.channel_id] = 0
+def run_counter():
+    local.last_post_number = 0
     server_mode = "count"
     full_prices = []
     previous_price = None
     while True:
-        msgs = extract_prices(fetch_function(server_mode, args))
+        msgs = extract_prices(fetch_function(server_mode))
         for msg in msgs:
             msg.calculate_and_set_rate_of_change(previous_price)
             full_prices.insert(0, msg)
             logger.info(f"accumulated {len(full_prices)} prices")
             previous_price = msg
-            if args.count == len(full_prices):
+            if local.count == len(full_prices):
                 break
 
-        if args.count == len(full_prices):
+        if local.count == len(full_prices):
             break
 
     server_ret = list(map(lambda price: price.get_json_data(), full_prices))
