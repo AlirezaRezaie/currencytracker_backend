082f4edceb54b56624f97ac1bb6397a47e26aaf0
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-19 22:37:53 +0330

    no need for this function since we have locals

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import re
 
 # Define a hardcoded admin username and password (for demonstration purposes)
 ADMIN_USERNAME = "admin"
@@ -12,20 +13,3 @@ def get_host():
 
 def get_port():
     return os.getenv("PORT", default=5000)
-
-
-def get_defaults(code=None):
-    default_channels = {
-        "USD": "nerkhedollarr",
-        "EUR": "sarafha_rate",
-        # "JPY": "",
-        # "GDP": "",
-        "DHS": "DHS_Dirham",
-    }
-    if not code:
-        return default_channels
-
-    if code in default_channels:
-        return default_channels[code]
-    else:
-        None
