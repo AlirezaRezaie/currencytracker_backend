6bb8b08ecc41a43fb77d3de36a626a9ae7170b48
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-19 22:37:01 +0330

    define defaults in locals

--- backend/locals.py
+++ backend/locals.py
@@ -2,3 +2,24 @@ import threading
 
 
 local = threading.local()
+local.default_channels = {
+    "USD": [
+        {
+            "channel_name": "nerkhedollarr",
+            "regex": r"(فردایی|نقدی|نـــقـدی|نـــقـدۍ|پایان معاملات).*?(\d{1,3}(?:,\d{3})*).(\w*)",
+            "nonstop": True,
+        },
+        {
+            "channel_name": "dollar_tehran3bze",
+            "regex": r"(فردایی|نقدی|نـــقـدی|نـــقـدۍ|پایان معاملات).*?(\d{1,3}(?:,\d{3})*).(\w*)",
+            "nonstop": True,
+        },
+        {"channel_name": "sarafha_rate", "regex": "", "nonstop": True},
+    ],
+    "EUR": [
+        {"channel_name": "sarafha_rate", "regex": "", "nonstop": True},
+    ],
+    # "JPY": "",
+    # "GDP": "",
+    "DHS": [{"channel_name": "DHS_Dirham", "regex": "", "nonstop": True}],
+}
