70f60001c87937fd4d8dc7b7463c0facc1b51aea
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-09-20 20:02:05 +0330

    switched to using json for channels

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,6 @@
 import os
 import socket
-import re
+import json
 
 # Define a hardcoded admin username and password (for demonstration purposes)
 ADMIN_USERNAME = "admin"
@@ -13,3 +13,9 @@ def get_host():
 
 def get_port():
     return os.getenv("PORT", default=5000)
+
+
+def get_defaults():
+    # read the json file
+    with open("default-channels.json", "r") as f:
+        return json.load(f)
