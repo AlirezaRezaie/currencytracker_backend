c8a4a7a129b61bbe6e1e76a975ab5e1478d66d47
mohammad <m.r.adibi125@gmail.com>
2023-09-25 00:51:54 +0330

    We saved Iran time calculation function as a service

---
+++ frontend/Flutter/dollartracker/lib/services/get_time_for_iran.dart
@@ -0,0 +1,35 @@
+ String getTimeForIran(time) {
+    // Time strings in "HH:mm" format
+    String time1String = time;
+    String time2String = "03:30";
+    // Parse the time strings into Duration objects
+    List<String> time1Parts = time1String.split(':');
+    List<String> time2Parts = time2String.split(':');
+
+    int hours1 = int.parse(time1Parts[0]);
+    int minutes1 = int.parse(time1Parts[1]);
+
+    int hours2 = int.parse(time2Parts[0]);
+    int minutes2 = int.parse(time2Parts[1]);
+
+    Duration time1 = Duration(hours: hours1, minutes: minutes1);
+    Duration time2 = Duration(hours: hours2, minutes: minutes2);
+
+    // Add the two Duration objects together
+    Duration totalTime = time1 + time2;
+
+    // Ensure the total time does not exceed 24 hours (1 day)
+    if (totalTime.inHours >= 24) {
+      totalTime = Duration(hours: 23, minutes: 59); // Set it to 23:59
+    }
+
+    // Extract hours and minutes from the totalTime
+    int totalHours = totalTime.inHours;
+    int totalMinutes = totalTime.inMinutes.remainder(60);
+
+    // Format the result as "HH:mm"
+    String result =
+        '${totalHours.toString().padLeft(2, '0')}:${totalMinutes.toString().padLeft(2, '0')}';
+
+    return result;
+  }
