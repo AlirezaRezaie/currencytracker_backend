3540cadb6d11db267b25edb4b83de6d23fef3f27
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-10-23 00:02:41 +0330

    move after

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,12 +2,10 @@ from a2wsgi import ASGIMiddleware
 from server import app  # Import your FastAPI app.
 import uvicorn
 
+
+application = ASGIMiddleware(app)
 uvicorn.run(
     "server:app",
     host="0.0.0.0",
     port=5000,
-    # reload=True
 )
-
-
-application = ASGIMiddleware(app)
