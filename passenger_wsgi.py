5afad990abf208f4a9a1cc39059e61b6708b4add
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-10-23 20:00:53 +0330

    check if uvicorn proc exist preodically to ensure its always running

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,18 @@
 from a2wsgi import ASGIMiddleware
 from server import app  # Import your FastAPI app.
+import subprocess
+import psutil
+
+server_proc_exist = False
+for process in psutil.process_iter(attrs=["pid", "name"]):
+    if "uvicorn" in process.info["name"]:
+        print("process uvicorn exists")
+        server_proc_exist = True
+        break
+
+if not server_proc_exist:
+    script_path = "run_server.sh"
+    print("creating a new uvicorn process")
+    subprocess.call(["sh", script_path])
 
 application = ASGIMiddleware(app)
