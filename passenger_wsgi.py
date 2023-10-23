e95e5e6886f8f99f3f8fb2e9b35f1309429c42b0
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-10-23 19:25:22 +0330

    remove uvicorn

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,4 @@
 from a2wsgi import ASGIMiddleware
 from server import app  # Import your FastAPI app.
-import uvicorn
-import multiprocessing
 
-
-def start_server():
-    uvicorn.run(
-        "server:app",
-        host="0.0.0.0",
-        port=5000,
-    )
-
-
-multiprocessing.Process(target=start_server).start()
 application = ASGIMiddleware(app)
