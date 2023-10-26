c28b3e75d69f5ea5bb19c6e7261ab8229cb18a2c
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-10-26 13:30:29 +0330

    fix server run shell script

---
+++ check_server_up.sh
@@ -0,0 +1,10 @@
+source /home/ariascod/virtualenv/currency_tracker/3.11/bin/activate && cd /home/ariascod/currency_tracker
+
+# Check if a process named "uvicorn" is running
+if ! pgrep -f "uvicorn" > /dev/null; then
+    echo "No uvicorn currenctly running Creating a new uvicorn process"
+    # Run uvicorn with your desired parameters
+    nohup uvicorn server:app --host 0.0.0.0 --port 5000 &
+fi
+
+
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,16 +3,16 @@ from server import app  # Import your FastAPI app.
 import subprocess
 import psutil
 
-server_proc_exist = False
-for process in psutil.process_iter(attrs=["pid", "name"]):
-    if "uvicorn" in process.info["name"]:
-        print("process uvicorn exists")
-        server_proc_exist = True
-        break
+# server_proc_exist = False
+# for process in psutil.process_iter(attrs=["pid", "name"]):
+#    if "uvicorn" in process.info["name"]:
+#        print("process uvicorn exists")
+#        server_proc_exist = True
+#        break
 
-if not server_proc_exist:
-    script_path = "run_server.sh"
-    print("creating a new uvicorn process")
-    subprocess.Popen(["sh", script_path])
+# if not server_proc_exist:
+#    script_path = "run_server.sh"
+#    print("creating a new uvicorn process")
+#    subprocess.Popen(["sh", script_path])
 
 application = ASGIMiddleware(app)
